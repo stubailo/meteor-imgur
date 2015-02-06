dd7d5b420a912930c67a043751cfa65411d30941
Sashko Stubailo <sashko@mit.edu>
2015-02-06 12:15:36 -0800

    Bump package version for mashape

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "A package to image data to Imgur with one function call.",
-  version: "1.0.1",
+  version: "1.0.2",
   git: "https://github.com/stubailo/meteor-imgur",
   name: "simple:imgur"
 });
